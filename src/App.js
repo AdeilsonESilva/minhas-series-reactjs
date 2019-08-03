2a01956f35dcc858bd9cc87eb106aee60b2c9280
Adeilson Silva <adeilson@gmail.com>
2019-08-03 19:33:28 -0300

    - Minhas series - aula 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Generos from './components/Generos';
+import NovoGenero from './components/NovoGenero';
+import EditarGenero from './components/EditarGenero';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import axios from 'axios';
 
-function App() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Home</h1>
+  );
+};
+
+const Ajuda = () => {
+  return (
+    <div>
+      <h1>Ajuda</h1>
+      <p>
+        Rodar: node node_modules\minhas-series-server\index.js
+      </p>
     </div>
+  );
+};
+
+function App () {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios
+      .get('/api')
+      .then(res => setData(res.data));
+  }, []);
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/generos/novo' exact component={NovoGenero} />
+          <Route path='/generos/:id' exact component={EditarGenero} />
+          <Route path='/generos' exact component={Generos} />
+          <Route path='/ajuda' exact component={Ajuda} />
+        </Switch>
+        <pre>{JSON.stringify(data)}</pre>
+      </div>
+    </Router>
   );
 }
 
